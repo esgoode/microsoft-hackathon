0dc05b88384b784bb89182ab0779a460779b3a95
Ronald Ding <roding@microsoft.com>
2018-07-23 14:10:41 -0500

    Half working admin page

--- repository/handlers.js
+++ repository/handlers.js
@@ -1,13 +1,47 @@
+var _ = require('lodash');
+
+var activeSessions = [];
+
 var chat = function (data) {
 	console.log("From user session: " + data.sessionToken);
 	console.log(data.content);
+	var found = false;
+	var currentSession;
+	_.forEach(activeSessions, function (session) {
+		if (session.sessionToken == data.sessionToken) {
+			currentSession = session;
+			currentSession.messages.push(data);
+			found = true;
+		}
+	});
+	if (!found) {
+		var newSession = {
+			sessionToken: data.sessionToken,
+			manual: false,
+			messages: []
+		}
+		newSession.messages.push(data);
+		currentSession = newSession;
+		activeSessions.push(newSession);
+	}
+	//Handle data
+	if (data.content.toLowerCase().includes('ronald')) {
+		//Switch to manual
+		currentSession.manual = true;
+	}
 	var newMessage = {
 		type: 0,
 		who: 'Bot',
 		time: new Date(),
 		content: 'Your message: <' + data.content + '> was received by the server. This is the server responding.'
 	};
+	currentSession.messages.push(newMessage);
 	return newMessage;
 }
 
+var getActiveSessions = function () {
+	return activeSessions;
+}
+
 module.exports.chat = chat;
+module.exports.getActiveSessions = getActiveSessions;
